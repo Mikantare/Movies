d4a8bed25206835643b2aeab670e308359f8b5f1
Mikantare <abespalov33@gmail.com>
2021-10-07 21:36:10 +0300

    Initial commit

---
+++ app/src/main/java/com/bespalov/movies/activitys/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bespalov.movies.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bespalov.movies.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
